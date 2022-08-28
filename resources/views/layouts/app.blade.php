ea62e5a79b8436cec8117d5ad45be949372c322a
Sarwar Sikder <sarwar.sikder@kaicomsol.com>
2022-08-28 21:53:25 +0600

    git remove

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,9 +25,9 @@
 <div id="app">
     <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
         <div class="container">
-            <a class="navbar-brand" href="{{ url('/') }}">
+          {{--  <a class="navbar-brand" href="{{ url('/') }}">
                 {{ config('app.name', 'Laravel') }}
-            </a>
+            </a>--}}
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                     aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -56,6 +56,9 @@
                             </li>
                         @endif
                     @else
+                        <li class="nav-item">
+                            <a class="nav-link" id="home" href="javascript:void(0)">Home</a>
+                        </li>
                         <li class="nav-item">
                             <a class="nav-link" href="{{route('shop')}}">Shop</a>
                         </li>
